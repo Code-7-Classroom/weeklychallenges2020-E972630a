cbb86ba5406d9c8b4a9c90232bfb766dedc74e52
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-03-30 11:14:04 -0400

    checkpoint

---
+++ Array_Checkpoint/script.js
@@ -0,0 +1,17 @@
+//Name three JavaScript Array Methods
+Array.push
+Array.pop
+Array.shift
+
+//First Array Drinks
+
+var drinks = ['water', 'coffee', 'tea']
+console.log(drinks)
+//Second Array Foods
+
+var foods = ['apple', 'sandwich', 'chips']
+console.log(foods)
+//Combined meals
+
+var meals = [...drinks, ...foods]
+console.log()
