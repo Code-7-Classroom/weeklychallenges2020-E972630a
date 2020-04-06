841f98745702937d9cf80a75ece36d026d681e87
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-04-06 11:03:18 -0400

    commented some things

--- Ecommerce_Project/product.js
+++ Ecommerce_Project/product.js
@@ -1,35 +1,35 @@
-var lessExpensive = [];
-var moreExpensive = [];
-filterSelection("all");
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("fragrance");
-  if (c == "all")
-    c = "";
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1)
-      addClass(x[i], "show");
-  }
-}
-function addClass(element, name) {
-  var i, lessExpensive, moreExpensive;
-  lessExpensive = element.className.split(" ");
-  moreExpensive = name.split(" ");
-  for (i = 0; i < moreExpensive.length; i++) {
-    if (lessExpensive.indexOf(moreExpensive[i]) == -1) {
-      element.className += " " + moreExpensive[i];
-    }
-  }
-}
-function removeClass(element, name) {
-  var i, lessExpensive, moreExpensive;
-  lessExpensive = element.className.split(" ");
-  moreExpensive = name.split(" ");
-  for (i = 0; i < moreExpensive.length; i++) {
-    while (lessExpensive.indexOf(moreExpensive[i]) > -1) {
-      lessExpensive.splice(lessExpensive.indexOf(moreExpensive[i]), 1);
-    }
-  }
-  element.className = lessExpensive.join(" ");
-}
+// var lessExpensive = [];
+// var moreExpensive = [];
+// filterSelection("all");
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("fragrance");
+//   if (c == "all")
+//     c = "";
+//   for (i = 0; i < x.length; i++) {
+//     removeClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1)
+//       addClass(x[i], "show");
+//   }
+// }
+// function addClass(element, name) {
+//   var i, lessExpensive, moreExpensive;
+//   lessExpensive = element.className.split(" ");
+//   moreExpensive = name.split(" ");
+//   for (i = 0; i < moreExpensive.length; i++) {
+//     if (lessExpensive.indexOf(moreExpensive[i]) == -1) {
+//       element.className += " " + moreExpensive[i];
+//     }
+//   }
+// }
+// function removeClass(element, name) {
+//   var i, lessExpensive, moreExpensive;
+//   lessExpensive = element.className.split(" ");
+//   moreExpensive = name.split(" ");
+//   for (i = 0; i < moreExpensive.length; i++) {
+//     while (lessExpensive.indexOf(moreExpensive[i]) > -1) {
+//       lessExpensive.splice(lessExpensive.indexOf(moreExpensive[i]), 1);
+//     }
+//   }
+//   element.className = lessExpensive.join(" ");
+// }
--- Ecommerce_Project/script.js
+++ Ecommerce_Project/script.js
@@ -4,39 +4,39 @@ function carousel() {
   var images;
   var slide = document.getElementsByClassName("imageSlides");
   for (images = 0; images < slide.length; images++) {
-    slide[images].style.display = "none";  
+    slide[images].style.display = "none";
   }
   mySlideShow++;
-  if (mySlideShow > slide.length) {mySlideShow = 1}    
-  slide[mySlideShow-1].style.display = "block";  
+  if (mySlideShow > slide.length) { mySlideShow = 1 }
+  slide[mySlideShow - 1].style.display = "block";
   setTimeout(carousel, 2000);
 }
 
-function validation() {
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('e-mail').value;
-  var message = document.getElementById('message').value;
-  var error = document.getElementById('error_message');
-  let text;
+// function validation() {
+//   var name = document.getElementById('name').value;
+//   var email = document.getElementById('e-mail').value;
+//   var message = document.getElementById('message').value;
+//   var error = document.getElementById('error_message');
+//   var textError;
 
-  error_message.style.padding = '1rem';
+//   error_message.style.padding = '1rem';
 
-  if (name.length < 0){
-      textError = 'Please Enter Valid Name';
-      error_message.innerHTML = textError;
-      return false;
-  }
+//   if (name.length < 4) {
+//     textError = 'Please Enter Valid Name';
+//     error_message.innerHTML = textError;
+//     return false;
+//   }
 
-  if (email.indexOf("@") == -1 || email.length < 0){
-      textError = 'Please Enter Valid Email Address';
-      error_message.innerHTML = textError;
-      return false;
-  }
+//   if (email.indexOf("@") == -1 || email.length < 10) {
+//     textError = 'Please Enter Valid Email Address';
+//     error_message.innerHTML = textError;
+//     return false;
+//   }
 
-  if (message.length < 0){
-      textError = 'Please Enter A Detailed Message';
-      error_message.innerHTML = textError;
-      return false;
-  }
-}
+//   if (message.length < 5) {
+//     textError = 'Please Enter A Detailed Message';
+//     error_message.innerHTML = textError;
+//     return false;
+//   }
+// }
 
